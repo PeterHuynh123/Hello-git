d2e8ef461a027a0c58242cfee22bee40f8fd2b3f
Hoang Nguyen <38454021+hoangdesu@users.noreply.github.com>
2022-03-02 17:55:33 +0700

    Added =D

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This is my first commit on git:)
 
 
 ![octocat](octocat.png)
+
+=D
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 name = input("name?")
 print("Hello " + name + ". Nice to meet you!")
+
+for i in range(10):
+    print("=D")
